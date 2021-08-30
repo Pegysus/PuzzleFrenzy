1e959a8f0c2503ffbde33772592bf04d292e0804
Pegysus <maxyeh@gmail.com>
2021-08-30 14:49:17 -0700

    first commit for pong scene

---
+++ PuzzleFrenzy/Scenes/Pong/PongScene.swift
@@ -0,0 +1,25 @@
+//
+//  PongScene.swift
+//  PuzzleFrenzy
+//
+//  Created by Max Yeh on 8/30/21.
+//
+
+import Foundation
+import SpriteKit
+
+class PongScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        setupNodes()
+    }
+    
+}
+
+extension PongScene {
+    
+    func setupNodes() {
+        
+    }
+    
+}
